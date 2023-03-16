184803fbd80957c1a7d09c91b242db03a3faacfc
summert1m3 <40441464+summert1m3@users.noreply.github.com>
2023-03-16 22:00:34 +0300

    Update Language.cs

--- src/WorkerService/ITHell.VacancyParser/ITHell.VacancyParser.Domain/Common/Language/Language.cs
+++ src/WorkerService/ITHell.VacancyParser/ITHell.VacancyParser.Domain/Common/Language/Language.cs
@@ -2,55 +2,248 @@ namespace ITHell.VacancyParser.Domain.Common.Language;
 
 public enum Language
 {
-    Unknown,
-    [MultipleDescription("Russian", "Русский")]
-    Russian,
-    [MultipleDescription("English", "Английский")]
+    [MultipleDescription("Английский", "English")]
     English,
-    [MultipleDescription("Kazakh", "Казахский")]
-    Kazakh,
-    [MultipleDescription("Belarusian", "Белорусский")]
-    Belarusian,
-    [MultipleDescription("German", "Немецкий")]
+
+    [MultipleDescription("Немецкий", "German")]
     German,
-    [MultipleDescription("French", "Французский")]
+
+    [MultipleDescription("Французский", "French")]
     French,
-    [MultipleDescription("Italian", "Итальянский")]
-    Italian,
-    [MultipleDescription("Spanish", "Испанский")]
-    Spanish,
-    [MultipleDescription("Portuguese", "Португальский")]
-    Portuguese,
-    [MultipleDescription("Polish", "Польский")]
-    Polish,
-    [MultipleDescription("Ukrainian", "Украинский")]
-    Ukrainian,
-    [MultipleDescription("Czech", "Чешский")]
-    Czech,
-    [MultipleDescription("Slovak", "Словацкий")]
-    Slovak,
-    [MultipleDescription("Bulgarian", "Болгарский")]
-    Bulgarian,
-    [MultipleDescription("Hungarian", "Венгерский")]
-    Hungarian,
-    [MultipleDescription("Romanian", "Румынский")]
-    Romanian,
-    [MultipleDescription("Serbian", "Сербский")]
-    Serbian,
-    [MultipleDescription("Croatian", "Хорватский")]
-    Croatian,
-    [MultipleDescription("Slovenian", "Словенский")]
-    Slovenian,
-    [MultipleDescription("Macedonian", "Македонский")]
-    Macedonian,
-    [MultipleDescription("Albanian", "Албанский")]
+
+    [MultipleDescription("Абазинский", "Abazin")]
+    Abazin,
+
+    [MultipleDescription("Абхазский", "Abkhazian")]
+    Abkhazian,
+
+    [MultipleDescription("Аварский", "Avaric")]
+    Avaric,
+
+    [MultipleDescription("Азербайджанский", "Azerbaijani")]
+    Azerbaijani,
+
+    [MultipleDescription("Албанский", "Albanian")]
     Albanian,
-    [MultipleDescription("Kyrgyz", "Кыргызский")]
+
+    [MultipleDescription("Амхарский", "Amharic")]
+    Amharic,
+
+    [MultipleDescription("Арабский", "Arabic")]
+    Arabic,
+
+    [MultipleDescription("Армянский", "Armenian")]
+    Armenian,
+
+    [MultipleDescription("Африкаанс", "Afrikaans")]
+    Afrikaans,
+
+    [MultipleDescription("Баскский", "Basque")]
+    Basque,
+
+    [MultipleDescription("Башкирский", "Bashkir")]
+    Bashkir,
+
+    [MultipleDescription("Белорусский", "Belarusian")]
+    Belarusian,
+
+    [MultipleDescription("Бенгальский", "Bengali")]
+    Bengali,
+
+    [MultipleDescription("Бирманский", "Burmese")]
+    Burmese,
+
+    [MultipleDescription("Болгарский", "Bulgarian")]
+    Bulgarian,
+
+    [MultipleDescription("Боснийский", "Bosnian")]
+    Bosnian,
+
+    [MultipleDescription("Бурятский", "Buryat")]
+    Buryat,
+
+    [MultipleDescription("Венгерский", "Hungarian")]
+    Hungarian,
+
+    [MultipleDescription("Вьетнамский", "Vietnamese")]
+    Vietnamese,
+
+    [MultipleDescription("Голландский", "Dutch")]
+    Dutch,
+
+    [MultipleDescription("Греческий", "Greek")]
+    Greek,
+
+    [MultipleDescription("Грузинский", "Georgian")]
+    Georgian,
+
+    [MultipleDescription("Дагестанский", "Dagestani")]
+    Dagestani,
+
+    [MultipleDescription("Даргинский", "Dargin")]
+    Dargin,
+
+    [MultipleDescription("Датский", "Danish")]
+    Danish,
+
+    [MultipleDescription("Иврит", "Hebrew")]
+    Hebrew,
+
+    [MultipleDescription("Ингушский", "Ingush")]
+    Ingush,
+
+    [MultipleDescription("Индонезийский", "Indonesian")]
+    Indonesian,
+
+    [MultipleDescription("Ирландский", "Irish")]
+    Irish,
+
+    [MultipleDescription("Исландский", "Icelandic")]
+    Icelandic,
+
+    [MultipleDescription("Испанский", "Spanish")]
+    Spanish,
+
+    [MultipleDescription("Итальянский", "Italian")]
+    Italian,
+
+    [MultipleDescription("Кабардино-черкесский", "Kabardian")]
+    Kabardian,
+    
+    [MultipleDescription("Казахский", "Kazakh")]
+    Kazakh,
+    [MultipleDescription("Карачаево-балкарский", "Karachay-Balkar")]
+    KarachayBalkar,
+    [MultipleDescription("Карельский", "Karelian")]
+    Karelian,
+    [MultipleDescription("Каталанский", "Catalan")]
+    Catalan,
+    [MultipleDescription("Кашмирский", "Kashmiri")]
+    Kashmiri,
+    [MultipleDescription("Китайский", "Chinese")]
+    Chinese,
+    [MultipleDescription("Коми", "Komi")]
+    Komi,
+    [MultipleDescription("Корейский", "Korean")]
+    Korean,
+    [MultipleDescription("Креольский (Сейшельские острова)", "Seychelles Creole")]
+    SeychellesCreole,
+    [MultipleDescription("Кумыкский", "Kumyk")]
+    Kumyk,
+    [MultipleDescription("Курдский", "Kurdish")]
+    Kurdish,
+    [MultipleDescription("Кхмерский (Камбоджийский)", "Khmer")]
+    Khmer,
+    [MultipleDescription("Кыргызский", "Kyrgyz")]
     Kyrgyz,
-    [MultipleDescription("Uzbek", "Узбекский")]
-    Uzbek,
-    [MultipleDescription("Turkmen", "Туркменский")]
+    [MultipleDescription("Лакский", "Lak")]
+    Lak,
+    [MultipleDescription("Лаосский", "Lao")]
+    Lao,
+    [MultipleDescription("Латинский", "Latin")]
+    Latin,
+    [MultipleDescription("Латышский", "Latvian")]
+    Latvian,
+    [MultipleDescription("Лезгинский", "Lezgian")]
+    Lezgian,
+    [MultipleDescription("Литовский", "Lithuanian")]
+    Lithuanian,
+    [MultipleDescription("Македонский", "Macedonian")]
+    Macedonian,
+    [MultipleDescription("Малазийский", "Malay")]
+    Malay,
+    [MultipleDescription("Мансийский", "Mansi")]
+    Mansi,
+    [MultipleDescription("Марийский", "Mari")]
+    Mari,
+    [MultipleDescription("Монгольский", "Mongolian")]
+    Mongolian,
+    [MultipleDescription("Непальский", "Nepali")]
+    Nepali,
+    [MultipleDescription("Ногайский", "Nogai")]
+    Nogai,
+    [MultipleDescription("Норвежский", "Norwegian")]
+    Norwegian,
+    [MultipleDescription("Осетинский", "Ossetian")]
+    Ossetian,
+    [MultipleDescription("Панджаби", "Punjabi")]
+    Punjabi,
+    [MultipleDescription("Персидский", "Persian")]
+    Persian,
+    [MultipleDescription("Польский", "Polish")]
+    Polish,
+    [MultipleDescription("Португальский", "Portuguese")]
+    Portuguese,
+    [MultipleDescription("Пушту", "Pashto")]
+    Pashto,
+    [MultipleDescription("Румынский", "Romanian")]
+    Romanian,
+    [MultipleDescription("Русский", "Russian")]
+    Russian,
+    [MultipleDescription("Санскрит", "Sanskrit")]
+    Sanskrit,
+    [MultipleDescription("Сербский", "Serbian")]
+    Serbian,
+    [MultipleDescription("Словацкий", "Slovak")]
+    Slovak,
+    [MultipleDescription("Словенский", "Slovenian")]
+    Slovenian,
+    [MultipleDescription("Сомалийский", "Somali")]
+    Somali,
+    [MultipleDescription("Суахили", "Swahili")]
+    Swahili,
+    [MultipleDescription("Тагальский", "Tagalog")]
+    Tagalog,
+    [MultipleDescription("Таджикский", "Tajik")]
+    Tajik,
+    [MultipleDescription("Тайский", "Thai")]
+    Thai,
+    [MultipleDescription("Талышский", "Talysh")]
+    Talysh,
+    [MultipleDescription("Тамильский", "Tamil")]
+    Tamil,
+    [MultipleDescription("Татарский", "Tatar")]
+    Tatar,
+    [MultipleDescription("Тибетский", "Tibetan")]
+    Tibetan,
+    [MultipleDescription("Тувинский", "Tuvinian")]
+    Tuvinian,
+    [MultipleDescription("Турецкий", "Turkish")]
+    Turkish,
+    [MultipleDescription("Туркменский", "Turkmen")]
     Turkmen,
-    [MultipleDescription("Azerbaijanian", "Азербайджанский")]
-    Azerbaijanian
+    [MultipleDescription("Удмуртский", "Udmurt")]
+    Udmurt,
+    [MultipleDescription("Узбекский", "Uzbek")]
+    Uzbek,
+    [MultipleDescription("Уйгурский", "Uyghur")]
+    Uyghur,
+    [MultipleDescription("Украинский", "Ukrainian")]
+    Ukrainian,
+    [MultipleDescription("Урду", "Urdu")]
+    Urdu,
+    [MultipleDescription("Финский", "Finnish")]
+    Finnish,
+    [MultipleDescription("Фламандский", "Flemish")]
+    Flemish,
+    [MultipleDescription("Хинди", "Hindi")]
+    Hindi,
+    [MultipleDescription("Хорватский", "Croatian")]
+    Croatian,
+    [MultipleDescription("Чеченский", "Chechen")]
+    Chechen,
+    [MultipleDescription("Чешский", "Czech")]
+    Czech,
+    [MultipleDescription("Чувашский", "Chuvash")]
+    Chuvash,
+    [MultipleDescription("Шведский", "Swedish")]
+    Swedish,
+    [MultipleDescription("Эсперанто", "Esperanto")]
+    Esperanto,
+    [MultipleDescription("Эстонский", "Estonian")]
+    Estonian,
+    [MultipleDescription("Якутский", "Yakut")]
+    Yakut,
+    [MultipleDescription("Японский", "Japanese")]
+    Japanese
 }
